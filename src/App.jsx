02bea7d64c410ea2f362dbf5bf15119d075ac4df
Kriti Pansari <KRITIPANSARI2815@GMAIL.COM>
2025-11-13 14:37:03 +0530

    11

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import DigitalClock from './components/DigitalClock'
+import Footer from './components/Footer';
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <DigitalClock />  
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
